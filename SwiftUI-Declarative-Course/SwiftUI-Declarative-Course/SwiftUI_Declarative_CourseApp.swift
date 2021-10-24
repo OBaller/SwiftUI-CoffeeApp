f230719fa3f45a9dfb2a04e7df740f93ea355c89
brightsummit.co <naseemoyebola@gmail.com>
2021-10-24 15:22:24 +0100

    folder setUp

---
+++ SwiftUI-Declarative-Course/SwiftUI-Declarative-Course/SwiftUI_Declarative_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Declarative_CourseApp.swift
+//  SwiftUI-Declarative-Course
+//
+//  Created by IBE on 24/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Declarative_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
